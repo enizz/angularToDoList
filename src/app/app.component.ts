cd1b76325a02995ac9e384fe945de9933196fc27
Eugene <Eugene.Nisnevich@Gmail.com>
2022-11-01 20:20:26 -0400

    some updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToDo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-to-do';
+  toDos: ToDo[] = [
+    {
+    task: "finish escalation database",
+    completed: false
+    },
+    {
+    task: "get attending physician form signed",
+    completed: false
+    },
+    {
+    task: "call UNUM",
+    completed: true  
+    }
+  ]
+  addTask(x: NgForm): void{
+    let newTask: ToDo = {
+      task: x.form.value,
+      completed: false
+    }
+    this.toDos.push
+  }
+  completeTask(x: ToDo): void{
+    x.completed = true
+    console.log(x.task)
+    console.log(x.completed)
+  }
 }
